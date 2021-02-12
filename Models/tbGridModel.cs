64fd2af17204dc3e74a84ce9934b947084608e05
LinaMaSe <karo_14sp@hotmail.com>
2021-02-12 11:08:53 -0600

    Add project files.

---
+++ Models/tbGridModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlugginCreation.Models
+{
+    public class tbGridModel
+    {
+        public int DateID { get; set; }
+        public System.DateTime StartDate { get; set; }
+        public System.DateTime EndDate { get; set; }
+        public bool Bootstrap { get; set; }
+        public string BootstrapVersion { get; set; }
+    }
+}
---
+++ Scripts/jquery.improvedGrid.js
@@ -0,0 +1,26 @@
+﻿(function ($) {
+    //declaracion de plug-in
+    $.fn.textoAzul = function (options) {
+        //obtenemos los parametros
+        options = $.extend({}, $.fn.textoAzul.defaultOptions, options);
+
+        this.each(function () {
+            var element = $(this);
+            element.addClass(options.elementClass);
+            element.css('color', 'blue');
+        });
+        return this;
+
+        //parametros del plugin
+        $.fn.textoAzul.defaultOptions = {
+            elementClass: 'texto-azul'
+        }
+
+        $(document).ready(function () {
+            //plugin sin/con parametros
+            $('p').textoAzul();
+        });
+    }
+
+})(jQuery);
+
